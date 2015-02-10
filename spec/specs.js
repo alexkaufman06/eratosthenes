425e8f8cdfe17ebfa5ce4edbe5b5db90699d5ab9
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-10 13:44:45 -0800

    Add initial files, specs, and methods

---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+describe("prime", function() {
+  it("it returns the number 2", function() {
+    expect(prime(2)).to.eql([2]);
+  });
+
+  it("returns [2, 3] for upper limit of 3", function() {
+    expect(prime(3)).to.eql([2, 3]);
+  });
+});
